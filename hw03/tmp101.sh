48b331e883f9c5ab825d47a5587b93a66611c629
root <root@beaglebone.localdomain>
2018-09-13 07:00:08 +0000

    Added param count check

--- hw03/tmp101.sh
+++ hw03/tmp101.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
 T1=$(echo "$(( `i2cget -y $1 $2 00` )) * 1.8 + 32" | bc)
 T2=$(echo "$(( `i2cget -y $1 $3 00` )) * 1.8 + 32" | bc)
 
