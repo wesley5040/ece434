a3e47dd3b3c47e64f4ecc282ed27243481754faf
root <root@beaglebone.localdomain>
2018-10-16 12:43:14 +0000

    tweaked pwm thing

--- hw09/pwm-generator/pwm1.c
+++ hw09/pwm-generator/pwm1.c
@@ -2,6 +2,11 @@
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
 
+#define P9_27 (1<<19)
+
+//unsigned int volatile * const GPIO3_CLEAR = (unsigned int *) 0x481AE190;
+//unsigned int volatile * const GPIO3_SET   = (unsigned int *) 0x481AE194;
+
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
@@ -12,12 +17,14 @@ void main(void)
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	gpio = 1<<19; // Select which pin to toggle.
+	gpio = 1<<5; // Select which pin to toggle.
 
 	while (1) {
 		__R30 |= gpio;
-		__delay_cycles(100000000);
+//		*GPIO3_SET   = P9_27;
+		__delay_cycles(1);
 		__R30 &= ~gpio;
-		__delay_cycles(100000000);
+//		*GPIO3_CLEAR = P9_27;
+		__delay_cycles(1);
 	}
 }
