e0da11229dbab26f3ed197f8823d3eafa23a5e46
Wesley Van Pelt <wcvp@me.com>
2018-10-22 17:30:45 -0400

    added part 5

---
+++ hw09/reading-input/input1.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <pru_cfg.h>
+#include "resource_table_empty.h"
+
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+void main(void)
+{
+	uint32_t led;
+	uint32_t sw;
+
+	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	led = 0x1;  // P9_29
+	sw  = 0x2;  // P9_30
+
+	while (1) {
+		if((__R31 & sw) == sw)
+			__R30 |= led;  // Turn on LED
+		else
+			__R30 &= ~led; // Turn off LED
+	}
+}
